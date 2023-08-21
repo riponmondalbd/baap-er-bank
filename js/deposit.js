cba6ff55fce956ffa297f69d86f6813ae71b6b94
Ripon Mondal <riponmondal149@gmail.com>
2023-08-21 06:41:59 +0600

    money withdraw and reduce balance for withdraw

--- js/deposit.js
+++ js/deposit.js
@@ -3,12 +3,31 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     // get the deposit amount from the  deposit input field
     // for input field use. value to the value inside the input field
     const depositField = document.getElementById('deposit-field');
-    const depositAmount = depositField.value;
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
 
     // get the current deposit total 
     // for no-input(element other than input, textarea) use innerText to get the text
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText = depositAmount;
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+
+    // add numbers to set the total deposit
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+    // const currentDepositTotal = parseFloat(previousDepositTotalString) + parseFloat(newDepositAmountString);
+    depositTotalElement.innerText = currentDepositTotal;
+
+
+    //get balance current total
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    // calculate current total balance
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotalElement.innerText = currentBalanceTotal;
+
+    // clear the deposit field
+    depositField.value = '';
 
 })
