a48820bc5e1995a0d644fbe739125a8d04eb6522
Ripon Mondal <riponmondal149@gmial.com>
2024-06-06 23:03:22 +0600

    get user from deposit and set deposit total value to input

---
+++ js/deposit.js
@@ -0,0 +1,9 @@
+document.getElementById('btn-deposit').addEventListener('click', function () {
+    const depositField = document.getElementById('deposit-field');
+    const depositAmount = depositField.value;
+
+    const depositTotalElement = document.getElementById('deposit-total');
+    const depositTotal = depositTotalElement.innerText;
+
+
+})
