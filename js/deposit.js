be62fedac5b988c2670b110cecf046840ef76916
Ripon Mondal <riponmondal149@gmial.com>
2024-06-06 23:25:46 +0600

    update deposit and balance and handle string add

--- js/deposit.js
+++ js/deposit.js
@@ -1,9 +1,32 @@
+// add event listener
 document.getElementById('btn-deposit').addEventListener('click', function () {
+    // get deposit field from input 
     const depositField = document.getElementById('deposit-field');
-    const depositAmount = depositField.value;
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
 
+    // get deposit total from main account
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal = depositTotalElement.innerText;
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
 
+    // add numbers to set the total deposit
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
 
+    // set deposit total
+    depositTotalElement.innerText = currentDepositTotal;
+
+    // get balance total
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    // add numbers to set the total balance
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+
+    // set balance total
+    balanceTotalElement.innerText = currentBalanceTotal;
+
+    // clear the deposit field
+    depositField.value = '';
 })
